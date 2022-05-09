7de229b7295d518a5b83b1784de0d8ffb3afca6f
zionjke <vincere802@gmail.com>
2022-05-09 15:48:29 +0300

    init commit

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+export type BloggerType = {
+    id: number
+    name: string
+    youtubeUrl: string
+}
+
+export type PostType = {
+    id: number,
+    title: string
+    shortDescription: string
+    content: string
+    bloggerId: number
+    bloggerName: string
+}
+
+export const BLOGGERS: BloggerType[] = [
+    {id: 0, name: 'Artem', youtubeUrl: 'https://www.youtube.com/c/satansdeer1'},
+    {id: 1, name: 'Dima', youtubeUrl: 'https://www.youtube.com/c/satansdeer2'},
+    {id: 2, name: 'Yan', youtubeUrl: 'https://www.youtube.com/c/satansdeer3'},
+    {id: 3, name: 'Sasha', youtubeUrl: 'https://www.youtube.com/c/satansdeer4'},
+]
+
+export const POSTS: PostType[] = [
+    {
+        id: 0,
+        title: 'testTitle0',
+        bloggerId: 0,
+        bloggerName: 'Artem',
+        content: 'Test Content0',
+        shortDescription: 'Test Description0'
+    },
+    {
+        id: 1,
+        title: 'testTitle1',
+        bloggerId: 1,
+        bloggerName: 'Dima',
+        content: 'Test Content1',
+        shortDescription: 'Test Description1'
+    },
+    {
+        id: 2,
+        title: 'testTitle2',
+        bloggerId: 2,
+        bloggerName: 'Yan',
+        content: 'Test Content2',
+        shortDescription: 'Test Description2'
+    },
+    {
+        id: 3,
+        title: 'testTitle3',
+        bloggerId: 3,
+        bloggerName: 'Sasha',
+        content: 'Test Content3',
+        shortDescription: 'Test Description3'
+    },
+
+]
---
+++ src/validation.ts
@@ -0,0 +1,27 @@
+type ErrorsMessagesType = {
+    errorsMessages: [
+        {
+            message: string,
+            field: string,
+        }
+
+    ],
+    resultCode: number
+}
+
+export const sendError = (field:string,message:string): ErrorsMessagesType => {
+    return {
+        errorsMessages: [
+            {
+                message: message,
+                field: field
+            }
+
+        ],
+        resultCode: 0
+    }
+}
+
+export const validateUrl = (url:string) => {
+    return /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(url);
+}
