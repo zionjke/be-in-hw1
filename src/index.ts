2fbaeae9b72da07a1c0d6c651fbbcbb8295285f9
zionjke <vincere802@gmail.com>
2022-05-15 17:15:01 +0300

    add basic auth

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,11 @@ import cors from 'cors'
 import {bloggersRouter} from "./routes/bloggers-router";
 import {postsRouter} from "./routes/posts-router";
 
-import {authMiddleware} from "./middlewares/auth-middleware";
-import { checkContentType } from './middlewares/checkContent-middleware';
-
-
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(authMiddleware)
 // app.use(checkContentType('application/json'))
 
 app.get('/', (req: Request, res: Response) => {
