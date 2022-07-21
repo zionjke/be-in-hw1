789f06cccdab0b0783b8401809409b8eda57afd9
Vasyliev Artem <vincere802@gmail.com>
2022-07-21 18:57:35 +0300

    add limiter

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -2,15 +2,15 @@ import {Router} from "express";
 
 import {authController} from "../controllers/auth-controller";
 import {registrationCodeValidation, userValidation, validationMiddleware} from "../middlewares/validationMiddleware";
-import {authLimiter} from "../utils/limiter";
+
 
 export const authRouter = Router();
 
 authRouter
     .post('/login', authController.login)
 
-    .post('/registration', userValidation, validationMiddleware, authLimiter, authController.registration)
+    .post('/registration', userValidation, validationMiddleware, authController.registration)
 
-    .post('/registration-confirmation', registrationCodeValidation, validationMiddleware, authLimiter, authController.confirmRegistration)
+    .post('/registration-confirmation', registrationCodeValidation, validationMiddleware, authController.confirmRegistration)
 
-    .post('/registration-email-resending', userValidation[2], validationMiddleware, authLimiter, authController.emailResending)
+    .post('/registration-email-resending', userValidation[2], validationMiddleware, authController.emailResending)
