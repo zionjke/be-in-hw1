26331eff5deab3461e5c037dce1c76c3564f0aac
Vasyliev Artem <vincere802@gmail.com>
2022-07-21 22:39:17 +0300

    add limiter

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.get('/', (req: Request, res: Response) => {
 
 //with NativeMongo
 app.use('/users', usersRouter)
-app.use('/auth', authRouter)
+app.use('/auth', authLimiter, authRouter)
 app.use('/bloggers', bloggersRouter)
 app.use('/posts', postsRouter)
 app.use('/comments', commentsRouter)
