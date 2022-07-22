a75eab21055e829751c1e40be889a0a62681a6cd
Vasyliev Artem <vincere802@gmail.com>
2022-07-22 17:06:52 +0300

    add limiter

--- src/utils/limiter.ts
+++ src/utils/limiter.ts
@@ -1,6 +1,10 @@
 import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
-    windowMs: 10000 ,
-    max: 5
+    windowMs: 10000,
+    max: 5,
+    message:
+        'Too many accounts created from this IP, please try again after an 10 second',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
