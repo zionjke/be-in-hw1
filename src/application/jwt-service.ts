19b0ca1ae97915d48c8e54c2d16e7eaf4827f835
Vasyliev Artem <vincere802@gmail.com>
2022-07-30 13:40:42 +0300

    add auth/me

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -4,8 +4,8 @@ import {tokensRepository} from "../repositories/tokens-repository";
 
 export const jwtService = {
     async generateToken(userId: string) {
-        const accessToken = jwt.sign({userId: userId}, SERVICE.JWT_ACCESS_KEY, {expiresIn: '30d'})
-        const refreshToken = jwt.sign({userId: userId}, SERVICE.JWT_REFRESH_KEY, {expiresIn: '30d'})
+        const accessToken = jwt.sign({userId: userId}, SERVICE.JWT_ACCESS_KEY, {expiresIn: '10s'})
+        const refreshToken = jwt.sign({userId: userId}, SERVICE.JWT_REFRESH_KEY, {expiresIn: '20s'})
 
         return {
             accessToken,
